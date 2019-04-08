648e197ea3a843967361808bf5c36643575ce94c
Skyler <ssorensen@toesas.com>
2019-04-08 09:20:31 -0600

    handled drag over and drag leave to show when files are hovered over dropzone

--- src/Main.elm
+++ src/Main.elm
@@ -4,7 +4,7 @@ import Browser
 import Css exposing (..)
 import Html.Styled exposing (Html, button, div, img, input, text)
 import Html.Styled.Attributes exposing (..)
-import Html.Styled.Events exposing (custom, on, onClick, preventDefaultOn, targetValue)
+import Html.Styled.Events exposing (custom, on, onClick, onSubmit, preventDefaultOn, targetValue)
 import Json.Decode as Json
 
 
@@ -31,6 +31,11 @@ onDragOver msg =
     preventDefaultOn "dragover" (Json.succeed ( msg, True ))
 
 
+onDragLeave : msg -> Html.Styled.Attribute msg
+onDragLeave msg =
+    preventDefaultOn "dragleave" (Json.succeed ( msg, True ))
+
+
 port fileSelected : { id : String, event : Json.Value } -> Cmd msg
 
 
@@ -38,12 +43,12 @@ port fileContentRead : (List ImagePortData -> msg) -> Sub msg
 
 
 type alias Model =
-    { count : Int, filesBase64 : List String }
+    { count : Int, filesBase64 : List String, draggingOver : String }
 
 
 initialModel : String -> ( Model, Cmd msg )
 initialModel flag =
-    ( { count = 0, filesBase64 = [] }, Cmd.none )
+    ( { count = 0, filesBase64 = [], draggingOver = "" }, Cmd.none )
 
 
 type Msg
@@ -51,6 +56,7 @@ type Msg
     | Decrement
     | HandleDrop Json.Value
     | FileRead (List ImagePortData)
+    | DragEnter String
     | DummyMsg
 
 
@@ -63,8 +69,11 @@ update msg model =
         Decrement ->
             ( { model | count = model.count - 1 }, Cmd.none )
 
-        HandleDrop string ->
-            ( model, fileSelected { id = "dropzone", event = string } )
+        HandleDrop eventJson ->
+            ( { model | draggingOver = "" }, fileSelected { id = "dropzone", event = eventJson } )
+
+        DragEnter id ->
+            ( { model | draggingOver = id }, Cmd.none )
 
         FileRead files ->
             let
@@ -85,9 +94,50 @@ subscriptions model =
 view : Model -> Html Msg
 view model =
     div [ css [ position relative, Css.width (px 500), Css.height (px 500) ] ]
-        [ input [ css [ Css.width (px 500), Css.height (px 500), opacity (num 0), zIndex (int 2) ], dropzone "true", onDrop HandleDrop, Html.Styled.Attributes.id "dropzone", onDragOver DummyMsg, type_ "file", Html.Styled.Attributes.accept "image/x-png,image/gif,image/jpeg,application/pdf", Html.Styled.Attributes.multiple True ]
+        [ input
+            [ css [ Css.width (px 500), Css.height (px 500), opacity (num 0), zIndex (int 2) ]
+            , dropzone "true"
+            , onDrop HandleDrop
+            , Html.Styled.Attributes.id "dropzone"
+            , if model.draggingOver == "dropzone" then
+                onSubmit DummyMsg
+
+              else
+                onDragOver (DragEnter "dropzone")
+            , onDragLeave (DragEnter "")
+            , type_ "file"
+            , Html.Styled.Attributes.accept "image/x-png,image/gif,image/jpeg,application/pdf"
+            , Html.Styled.Attributes.multiple True
+            ]
             []
-        , div [ css [ Css.width (px 500), position absolute, top (px 0), left (px 0), Css.height (px 500), border3 (px 2) dashed (hex "4a4a4a"), displayFlex, alignItems center, justifyContent center, pointerEvents none ] ] [ text "Drop File Here" ]
+        , div
+            [ css
+                [ Css.width (px 500)
+                , position absolute
+                , top (px 0)
+                , left (px 0)
+                , Css.height (px 500)
+                , border3
+                    (if model.draggingOver == "dropzone" then
+                        px 3
+
+                     else
+                        px 2
+                    )
+                    dashed
+                    (if model.draggingOver == "dropzone" then
+                        hex "374d91"
+
+                     else
+                        hex "4a4a4a"
+                    )
+                , displayFlex
+                , alignItems center
+                , justifyContent center
+                , pointerEvents none
+                ]
+            ]
+            [ text "Drop File Here" ]
         , div [] (List.map (\x -> img [ Html.Styled.Attributes.src x, css [ maxWidth (px 80) ] ] []) model.filesBase64)
         ]
 
